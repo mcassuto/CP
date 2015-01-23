c39cf9fc56a129d8feeb9010386091de0bfafc12
Marc Cassuto <bulk@k-ssuto.com>
2015-01-22 19:11:43 -0500

    product_pricelist_show_related_prices - Add new module

---
+++ product_pricelist_show_related_prices/product_pricelist_item.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Marc Cassuto <marc.cassuto@gmail.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+
+class ProductPricelistItem(osv.osv):
+    _inherit = "product.pricelist.item"
+
+
+    _columns = {
+        'list_price': fields.related('product_id',
+                                      'list_price',
+                                      type='float',
+                                      string='Default sale price',
+                                      readonly=True),
+        'standard_price': fields.related('product_id',
+                                      'standard_price',
+                                      type='float',
+                                      string='Cost',
+                                      readonly=True),
+        }
