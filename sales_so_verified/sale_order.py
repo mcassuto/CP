9429732c6ea633f7a36dd3d80c7e2369e98e20ed
Marc Cassuto <bulk@k-ssuto.com>
2015-02-10 22:21:31 -0500

    [sales_so_verified] ADD new module

---
+++ sales_so_verified/sale_order.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Marc Cassuto <marc.cassuto@gmail.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+
+class SaleOrder(osv.osv):
+    _inherit = "sale.order"
+
+    _columns = {
+        'verified' : fields.boolean(
+            'Verified ?',
+            help="It indicates that this sales order has been verified.",
+            track_visibility='onchange',
+            ),
+        'verification_note': fields.text(
+            'Note about the verification',
+            help="Please provide explanation about this verification.",
+            track_visibility='onchange',
+            )
+        }
