41f0692989c7d417f22debe666f1f93c68809e89
Marc Cassuto <bulk@k-ssuto.com>
2015-02-12 18:41:37 -0500

    [product_pricelist_show_related_prices] - remove old methods

--- product_pricelist_show_related_prices/product_pricelist_item.py
+++ product_pricelist_show_related_prices/product_pricelist_item.py
@@ -28,139 +28,6 @@ import openerp.addons.decimal_precision as dp
 class ProductPricelistItem(osv.osv):
     _inherit = "product.pricelist.item"
 
-    def get_list_price(self, cr, uid, ids, field_names=None, arg=False,
-                       context=None):
-        res = {}
-        if context is None:
-            context = {}
-
-        currency_obj = self.pool.get('res.currency')
-        product_obj = self.pool.get('product.product')
-        price_type_obj = self.pool.get('product.price.type')
-
-        for pvi in self.browse(cr, uid, ids, context=context):
-            product_id = pvi.product_id
-            pricelist_currency_id = \
-                pvi.price_version_id.pricelist_id.currency_id.id
-
-            # price_type = 'list_price' => parameter = 1
-            price_type = price_type_obj.browse(cr, uid, 1)
-
-            price = currency_obj.compute(
-                cr, uid,
-                price_type.currency_id.id,
-                pricelist_currency_id,
-                product_obj.price_get(cr, uid,
-                                      [product_id.id],
-                                      'list_price',
-                                      context=context
-                                      )[product_id.id],
-                round=False,
-                context=context)
-
-            res[pvi.id] = price
-        return res
-
-    def get_standard_price(self, cr, uid, ids, field_names=None, arg=False,
-                           context=None):
-        res = {}
-        if context is None:
-            context = {}
-        currency_obj = self.pool.get('res.currency')
-        product_obj = self.pool.get('product.product')
-        price_type_obj = self.pool.get('product.price.type')
-
-        for pvi in self.browse(cr, uid, ids, context=context):
-            product_id = pvi.product_id
-            pricelist_currency_id = \
-                pvi.price_version_id.pricelist_id.currency_id.id
-
-            # price_type = 'standard_price' => parameter = 2
-            price_type = price_type_obj.browse(cr, uid, 2)
-
-            price = currency_obj.compute(
-                cr, uid,
-                price_type.currency_id.id,
-                pricelist_currency_id,
-                product_obj.price_get(cr, uid,
-                                      [product_id.id],
-                                      'standard_price',
-                                      context=context
-                                      )[product_id.id],
-                round=False,
-                context=context)
-
-            res[pvi.id] = price
-        return res
-
-    def get_computed_price(self, cr, uid, ids, field_names=None, arg=False,
-                           context=None):
-        res = {}
-        if context is None:
-            context = {}
-
-        currency_obj = self.pool.get('res.currency')
-        product_obj = self.pool.get('product.product')
-        product_uom_obj = self.pool.get('product.uom')
-        price_type_obj = self.pool.get('product.price.type')
-
-        for pvi in self.browse(cr, uid, ids, context=context):
-            # Pricelist version item based on another pricelist
-            if pvi.base == -1:
-                price = False
-            # Pricelist version item based on Supplier price on the product
-            # form
-            elif pvi.base == -2:
-                price = False
-            # Pricelist version item based on any other pricetype
-            else:
-                product_id = pvi.product_id
-                pricelist_currency_id = \
-                    pvi.price_version_id.pricelist_id.currency_id.id
-                price_type = price_type_obj.browse(cr, uid, int(pvi.base))
-                # uom_price_already_computed = True
-                # currency_ctx = dict(context,
-                #                    currency_id = price_type.currency_id.id)
-                # price= product_id.price_get(price_type.field,
-                #                         context=currency_ctx)[product_id.id]
-                price = currency_obj.compute(
-                    cr, uid,
-                    price_type.currency_id.id,
-                    pricelist_currency_id,
-                    product_obj.price_get(cr, uid,
-                                          [product_id.id],
-                                          price_type.field,
-                                          context=context
-                                          )[product_id.id],
-                    round=False,
-                    context=context)
-
-                if price is not False:
-                    price_limit = price
-                    price *= (1.0 + pvi.price_discount or 0.0)
-                    if pvi.price_round:
-                        price = tools.float_round(
-                            price,
-                            precision_rounding=pvi.price_round
-                        )
-                    if context.get('uom'):
-                        # compute price_surcharge based on reference uom
-                        factor = product_uom_obj.browse(
-                            cr, uid,
-                            context.get('uom'),
-                            context=context
-                        ).factor
-                    else:
-                        factor = 1.0
-                    price += (pvi.price_surcharge or 0.0) / factor
-                    if pvi.price_min_margin:
-                        price = max(price, price_limit + pvi.price_min_margin)
-                    if pvi.price_max_margin:
-                        price = min(price, price_limit + pvi.price_max_margin)
-
-            res[pvi.id] = price
-        return res
-
     def _get_prices(self, cr, uid, ids, field_names, arg, context=None):
         res = {}
         if context is None:
@@ -258,7 +125,7 @@ class ProductPricelistItem(osv.osv):
 
             res[pvi.id]['computed_price'] = price
 
-            # COMPUTE margin (Computed Price - Cost) * 100 / Cost')
+            # COMPUTE margin (Computed Price - Cost) * 100 / Cost
             res[pvi.id]['margin'] = \
                 res[pvi.id]['standard_price'] and \
                 (res[pvi.id]['computed_price'] -
@@ -266,28 +133,22 @@ class ProductPricelistItem(osv.osv):
                  ) * 100 / res[pvi.id]['standard_price']
         return res
 
-
     _columns = {
         'list_price': fields.function(
-            # get_list_price,
             _get_prices,
             multi='prices',
             type='float',
-            digits_compute=dp.get_precision(
-                'Product Price'),
+            digits_compute=dp.get_precision('Product Price'),
             string=_('Default sale price'),
             readonly=True),
         'standard_price': fields.function(
-            # get_standard_price,
             _get_prices,
             multi='prices',
             type='float',
-            digits_compute=dp.get_precision(
-                'Product Price'),
+            digits_compute=dp.get_precision('Product Price'),
             string=_('Cost'),
             readonly=True),
         'computed_price': fields.function(
-            # get_computed_price,
             _get_prices,
             multi='prices',
             type='float',
